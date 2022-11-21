6554a34d30b751a004112b2206326b2f15f3e133
roldanmoncada <roldan.h.moncada@gmail.com>
2022-11-20 19:21:21 -0700

    Beginning to build the main site's html and style. Will finish this then move onto creating the templates for the different employee types.

---
+++ index.js
@@ -0,0 +1,10 @@
+//Declaring all global variables that needed to require files located elsewhere.
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
+// Declaring all dependencies
+const fs = require('fs');
+const path = require('path');
+
