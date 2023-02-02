b14b25d2f62fb3400b3a0515966ef164b729c2f8
roldanmoncada <roldan.h.moncada@gmail.com>
2023-02-01 19:06:22 -0700

    reworking some things.

---
+++ src/main-site.js
@@ -0,0 +1,61 @@
+// <!--Building out the main website's html and styling. There will be separate boilerplates for the inclusion of the different employee types.-->
+const generateTeam = teamData => {
+    const generateManager = managerData => {
+        return `
+        <div>
+            <div>
+                <h1>${managerData.getName()}</h1>
+                <h2>${managerData.getRole()}</h2>
+            </div>
+            <ul>
+                <li>${managerData.getId()}</li>
+                <li>${managerData.getEmail()}</li>
+                <li>${managerData.getOfficeNumber()}</li>
+            </ul>
+        </div>
+        `
+    } // remember to separate with comma!
+    // <!--CONTINUE PATTERN FOR OTHER ROLES, SAME STRUCTURE AND ERYTHING-->
+    const htmlData = []
+
+    htmlData.push(teamData).filter(employee => {
+        employee.getRole() === "Manager"
+    }).map(manager => {generateManager(manager)});
+
+    htmlData.push(teamData).filter(employee => {
+        employee.getRole() === "Engineer"
+    }).map(engineer => {generateManager(engineer)});
+    // <!--CONTINUE PATTERN FOR OTHER ROLES, SAME STRUCTURE AND ERYTHING-->
+
+    return htmlData.join('')
+}
+
+module.exports = teamData => {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+</head>
+<body>
+    <div>
+        <div>
+            <h1>My Special Team</h1>
+        </div>
+        <div>
+            <div>
+                ${generateTeam(teamData)}
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+    `
+}
+
+
